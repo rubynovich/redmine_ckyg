c0fc5f6ff906244a31e32b41bcf3bca9728fecbf
rubynovich <roman@shipiev.me>
2013-04-08 17:01:16 +0400

    ops

--- app/models/workplace_time.rb
+++ app/models/workplace_time.rb
@@ -5,5 +5,4 @@ class WorkplaceTime < ActiveRecord::Base
 
   validates_presence_of :user_id, :workday, :start_time, :duration, :delay
   validates_uniqueness_of :workday, :scope => :user_id
-  validates :workday, :date => true
 end
