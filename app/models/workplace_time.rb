192d658783b1f65cef8eba7902f967a17abfc579
rubynovich <roman@shipiev.me>
2013-04-08 16:27:26 +0400

    add: first commit

---
+++ app/models/workplace_time.rb
@@ -0,0 +1,9 @@
+class WorkplaceTime < ActiveRecord::Base
+  unloadable
+
+  belongs_to :user
+
+  validates_presence_of :user_id, :workday, :start_time, :duration, :delay
+  validates_uniqueness_of :workday, :scope => :user_id
+  validates :workday, :date => true
+end
